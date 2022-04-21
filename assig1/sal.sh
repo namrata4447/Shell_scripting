34f7564cd4b5e55b08f72ea6751bd08fe79520c9
namrata4447 <namratamansur@gmail.com>
2022-04-21 07:41:13 +0000

    update

---
+++ assig1/sal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#echo "Enter basic salary amount Rs:"
+#read b
+dp=$(( 50 * $b / 100 ))
+echo "dp is Rs:$dp"
+da=$((35 * ($b + $dp) / 100 ))
+echo "da is Rs:$da"
+hra=$((8 * ($b + $dp) / 100 ))
+echo "hra is Rs:$hra"
+ma=$((3 * ($b + $dp) / 100 ))
+echo "ma is Rs:$ma"
+pf=$((10 * ($b + $dp) / 100 ))
+echo "pf is Rs:$pf"
+salary=$(($b + $dp + $da + $hra + $ma- $pf ))
+echo "salary is Rs:$salary"
+
+
+
+
