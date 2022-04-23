cbf7b7b3bc054a244059474979f4ab1f6a1648ab
namrata4447 <namratamansur@gmail.com>
2022-04-23 05:49:30 +0000

    update

---
+++ assig1/saljenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#echo "Enter basic salary amount Rs:"
+#read b
+$b
+if [[ $b -gt 0 ]]
+then
+	dp=$(( 50 * $b / 100 ))
+	echo "dp is Rs:$dp"
+	da=$((35 * ($b + $dp) / 100 ))
+	echo "da is Rs:$da"
+	hra=$((8 * ($b + $dp) / 100 ))
+	echo "hra is Rs:$hra"
+	ma=$((3 * ($b + $dp) / 100 ))
+	echo "ma is Rs:$ma"
+	pf=$((10 * ($b + $dp) / 100 ))
+	echo "pf is Rs:$pf"
+	salary=$(($b + $dp + $da + $hra + $ma- $pf ))
+	echo "salary is Rs:$salary"
+else
+	echo "failed"
+fi
+
+
